0e617d384f3a1877ae17c5b441bd7826e0b61a10
unknown <akurom.studio@gmail.com>
2023-02-23 15:26:24 +0100

    changed damage bar design

--- lib/views/widgets/damage_snack.dart
+++ lib/views/widgets/damage_snack.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:mtg_roulette/const/color_constants.dart';
 import 'package:mtg_roulette/const/size_constants.dart';
@@ -13,9 +13,10 @@ const WIDGET_DECAY_MS = 2000;
 
 class DamageSnack extends StatefulWidget {
   final int initialCount;
+  final Alignment alignment;
   final CounterModel counter;
 
-  const DamageSnack({required this.initialCount, required this.counter});
+  const DamageSnack({required this.initialCount, required this.alignment, required this.counter});
 
   @override
   State<DamageSnack> createState() => _DamageSnackState();
@@ -29,7 +30,7 @@ class _DamageSnackState extends State<DamageSnack> {
   @override
   void initState() {
     _initialCount = widget.initialCount;
-    _visible = false;
+    _visible = true;
     /*_timer = Timer(Duration(milliseconds: WIDGET_DECAY_MS), () {
       _visible = false;
     });*/
@@ -55,54 +56,70 @@ class _DamageSnackState extends State<DamageSnack> {
 */
   }
 
-
   @override
   Widget build(BuildContext context) {
     print('snack built');
 
+    double width = screenWidth(context) * SizeConstants.damageSnackWidth;
+    double height = width;
 
     return ChangeNotifierProvider<CounterModel>.value(
       value: widget.counter,
       child: Consumer<CounterModel>(builder: (context, counter, child) {
-
         _refresh(counter.count);
 
-        return AnimatedOpacity(
-          // If the widget is visible, animate to 0.0 (invisible).
-          // If the widget is hidden, animate to 1.0 (fully visible).
-          opacity: _visible ? 1.0 : 0.0,
-          duration: const Duration(milliseconds: 200),
-          // The green box must be a child of the AnimatedOpacity widget.
-          child: Container(
-            color: ColorConstants.black,
-            width: screenWidth(context) * SizeConstants.damageSnackWidth,
-            height: screenWidth(context) * SizeConstants.damageSnackHeight,
-            child: /*OverflowBox(
-            alignment: Alignment.topCenter,
-            minHeight: 20,
-            minWidth: 20,
-            maxHeight: screenHeight(context) / 10,
-            maxWidth: screenHeight(context) / 10,
-            child: */
-                FractionallySizedBox(
-              widthFactor: 0.8,
-              child: FittedBox(
-                  /*child: RotationTransition(
-                    turns: AlwaysStoppedAnimation(-45 / 360),*/
-                  child: Text(
-                      '${counter.count > _initialCount ? "+" : counter.count < _initialCount ? "-" : ""} ${(counter.count - _initialCount).abs()}',
-                      style: Theme.of(context).textTheme.displaySmall!.copyWith(
-                            color: counter.count > _initialCount
-                                ? Colors.greenAccent
-                                : counter.count < _initialCount
-                                    ? Colors.red
-                                    : Colors.white,
-                          ))),
-            ),
-            //),
-            //),
-          ),
-        );
+        return SizedBox(
+            width: 0, height: 0,
+            child: OverflowBox(
+                alignment: Alignment.bottomCenter,
+                /*minHeight: 20,
+                minWidth: 20,*/
+                maxHeight: screenWidth(context) * SizeConstants.damageSnackWidth,
+                maxWidth: screenWidth(context) * SizeConstants.damageSnackWidth,
+                child: AnimatedOpacity(
+                  // If the widget is visible, animate to 0.0 (invisible).
+                  // If the widget is hidden, animate to 1.0 (fully visible).
+                  opacity: _visible ? 1.0 : 0.0,
+                  duration: const Duration(milliseconds: 200),
+                  // The green box must be a child of the AnimatedOpacity widget.
+                  child: Stack(
+                    children: [
+                      Container(
+                        width: width,
+                        height: height,
+                        color: ColorConstants.black,
+                        // ---
+                        transform: Matrix4.translationValues(width / 2, 1.0, 1.0) *
+                            Matrix4.diagonal3Values(1.0, 0.5, 1.0) *
+                            Matrix4.rotationZ(pi / 4),
+                        // ---
+                      ),
+                      Positioned(
+                        top: width / 8,
+                        child: Container(
+                          width: width,
+                          height: height / 2,
+                          child: FractionallySizedBox(
+                            widthFactor: 0.8,
+                            child: FittedBox(
+                              child: Text(
+                                '${counter.count > _initialCount ? "+" : counter.count < _initialCount ? "-" : ""} ${(counter.count - _initialCount).abs()}',
+                                style: Theme.of(context).textTheme.displaySmall!.copyWith(
+                                      fontWeight: FontWeight.bold,
+                                      color: counter.count > _initialCount
+                                          ? Colors.greenAccent
+                                          : counter.count < _initialCount
+                                              ? Colors.red
+                                              : Colors.white,
+                                    ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )));
       }),
     );
   }
